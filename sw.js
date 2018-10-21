80ecd122bb5f705afdb76ac6d4d01e598c68af2d
Vijay G <vjremo@gmail.com>
2018-10-20 22:29:03 -0400

    feat: Add source, update variables and add comments

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+//Referred source 'https://matthewcranford.com/restaurant-reviews-app-walkthrough-part-4-service-workers/' for below section
+
+//Listing files to be stored in cache for offline access
 const filesToCache = [
     '/',
     '/index.html',
@@ -13,30 +16,30 @@ const filesToCache = [
     '/img/7.jpg',
     '/img/8.jpg',
     '/img/9.jpg',
-    '/img/10.jpg',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js'
+    '/img/10.jpg'
   ];
 
+//This function adds all files to cache storage 
 self.addEventListener('install', function(event){
     event.waitUntil(
-        caches.open('v1').then(function(cache){
+        caches.open('rest-reviews-v1').then(function(cache){
             return cache.addAll(filesToCache);
         })
 
     );
 });
 
+//Checks if content is found in cache and returns to user
+//If content is not found in cache, fetches using 'Fetch' event and also adds that to cache storage
 self.addEventListener('fetch', function(event){
     event.respondWith(
-        caches.match(event.request).then(function (response) {
+        caches.match(event.request).then(function(response) {
             if(response){
                 return response;
             }else{
                 return fetch(event.request).then(function(response){
                     const clonedResponse = response.clone();
-                    caches.open('v1').then(function (cache) {
+                    caches.open('rest-reviews-v1').then(function(cache) {
                         cache.put(event.request, clonedResponse);
                     })
                     return response;
