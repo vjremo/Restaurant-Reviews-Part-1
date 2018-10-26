9c339e1ab06c15e881d20255dc49631044a7ef63
Vijay G <vjremo@gmail.com>
2018-10-26 00:26:57 -0400

    fix: Update alt text for images

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -157,7 +157,7 @@ class DBHelper {
    * Restaurant image alternative text.
    */
   static imageAltTextForRestaurant(restaurant) {
-    return (`/img/${restaurant.name}`);
+    return (`This is an picture of ${restaurant.name} restaurant`);
   }
 
   /**
@@ -167,7 +167,7 @@ class DBHelper {
     // https://leafletjs.com/reference-1.3.0.html#marker  
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
-      alt: restaurant.name,
+      alt: DBHelper.imageAltTextForRestaurant(restaurant),
       keyboard: false,
       url: DBHelper.urlForRestaurant(restaurant)
       })
